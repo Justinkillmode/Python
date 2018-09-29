94693b25bdcd0d9f3198557d3aae13699675854a
Justin <j_schepers@gmx.de>
2018-09-30 00:25:35 +0200

    Mehr Lernen

--- PythonScrips/HalloWelt.py
+++ PythonScrips/HalloWelt.py
@@ -1,1 +1,13 @@
 print('Hallo Welt!')
+t = True
+
+Liste = [1, 2, 3, 4]
+
+if t == True:
+    print(Liste)
+else:
+    print("t ist nicht True")
+
+Liste.append(14)
+
+print(Liste)
---
+++ PythonScrips/whilefor.py
@@ -0,0 +1,17 @@
+x = 0
+y = ["Python", "Java", "C++"]
+
+while( x < 42):
+    x = x + 1
+    print(x)
+
+for i in range(5, 10):
+    if i == 7:
+        break
+    print(i)
+    
+for w in y:
+    print(w)
+
+for i in range(5):
+    print(i)
